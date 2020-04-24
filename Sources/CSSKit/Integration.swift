92d48fe983b0e3e4e2b5a859390b920531e318df
Maxim Krouk <id.maximkrouk@gmail.com>
2020-04-24 15:45:28 +0300

    Improved integration generator to support versions from 1.0.0-alpha.3.1

--- Sources/CSSKit/Integration.swift
+++ Sources/CSSKit/Integration.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 #if DEBUG
+
 /// Should only be used for integration generations
 ///
 /// Builder function should have this signature:
@@ -19,14 +20,12 @@ import Foundation
 /// - Parameter builder: builder function of the target type
 /// - Parameter input: path to `Style+StaticFactory.swift`
 /// - Parameter output: path to output file
-@discardableResult
-@available(*, deprecated, message: "Should only be used for integration generations")
-public func generateIntegration(in type: String, builder: String, input: String, output: String) -> Bool {
+public func generateIntegration(in type: String, builder: String, implement: Bool, useReturn: Bool, input: String, output: String) -> Bool {
     let manager = FileManager.default
     guard let data = manager.contents(atPath: input) else { return false }
     guard let content = String(data: data, encoding: .utf8) else { return false }
     
-    let output = generateIntegration(in: type, builder: builder, from: content)
+    let output = generateIntegration(in: type, builder: builder, implement: implement, useReturn: useReturn, from: content)
     guard let outputData = output.data(using: .utf8) else { return false }
     
     return manager.createFile(atPath: output, contents: outputData, attributes: .none)
@@ -41,9 +40,11 @@ public func generateIntegration(in type: String, builder: String, input: String,
 ///
 /// - Parameter type: type to integrate the framework in
 /// - Parameter builder: builder function of the target type
+/// - Parameter implment: generate implementation header placeholder
+/// - Parameter useReturn: use return keywords in functions
+/// - Parameter builder: builder function of the target type
 /// - Parameter content: content of `Style+StaticFactory.swift` file
-@available(*, deprecated, message: "Should only be used for integration generations")
-public func generateIntegration(in type: String, builder: String, from content: String) -> String {
+public func generateIntegration(in type: String, builder: String, implement: Bool, useReturn: Bool, from content: String) -> String {
     var lines = content.components(separatedBy: .newlines)
     lines = Array(lines.drop(while: { !$0.contains("extension") }))
     
@@ -52,17 +53,32 @@ public func generateIntegration(in type: String, builder: String, from content: 
     
     lines.forEach { line in
         if line.contains("extension") {
+            if implement {
+                result.append("// MARK: - Implementation")
+                result.append("")
+                result.append("extension \(type) {")
+                result.append("")
+                result.append("    internal func \(builder)(_ style: Style) -> Self {")
+                result.append("        <#Implementation#>")
+                result.append("    }")
+                result.append("")
+                result.append("}")
+                result.append("")
+            }
+            result.append("// MARK: - Generated")
+            result.append("")
             result.append("extension \(type) {")
         } else if line.contains("public static func ") {
             result.append(line.replacingOccurrences(of: "public static func", with: "public func"))
             _identifier = String(line.trimmingCharacters(in: .whitespaces).dropFirst(19))
             _identifier = String(_identifier[_identifier.startIndex..<_identifier.firstIndex(of: "(")!])
-        } else if line.count > 5 {
-            result.append("        \(builder)(.\(_identifier)(value))")
+        } else if line.contains("return ") {
+            result.append("        \(useReturn ? "return " : "")\(builder)(.\(_identifier)(value))")
         } else {
             result.append(line)
         }
     }
     return result.joined(separator: "\n")
 }
+
 #endif
